a90788f8b329258631ccca34bf7d817b44980aaf
boo1ean <boo1ean0807@gmail.com>
2013-05-04 14:25:56 +0300

    Use fixtures generator, remove redundant helper methods

--- tests/lib/TestCase.php
+++ tests/lib/TestCase.php
@@ -3,6 +3,7 @@
 use App\Model\User;
 use App\Model\Business;
 use App\Model\Product;
+use App\Ext\Fixtures;
 
 class TestCase extends PHPUnit_Framework_TestCase
 {
@@ -27,6 +28,7 @@ class TestCase extends PHPUnit_Framework_TestCase
 
         $this->container = $this->app->getContainer();
         $this->faker     = Faker\Factory::create();
+        $this->fixtures  = new Fixtures();
     }
 
     public function setUp() {
@@ -35,83 +37,13 @@ class TestCase extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Creates user
-     * @return App\Model\Business
+     * Aggragate fixtures methods to TestCase
      */
-    public function createUser() {
-        return User::create(array(
-            'email'        => $this->faker->email,
-            'password'     => self::DEFAULT_PASSWORD,
-            'first_name'   => $this->faker->firstName,
-            'last_name'    => $this->faker->lastName,
-            'country'      => $this->faker->country,
-            'phone_number' => $this->faker->phoneNumber,
-            'city'         => $this->faker->city,
-            'address'      => $this->faker->address
-        ));
-    }
-
-    /**
-     * Create business
-     *
-     * @param integer $user_id business owner id 
-     * @return App\Model\Business
-     */
-    public function createBusiness($user_id, $attributes = array()) {
-        $attributes = array_merge(array(
-            'user_id'      => $user_id,
-            'name'         => $this->faker->company,
-            'phone_number' => $this->faker->phoneNumber
-        ), $attributes);
-
-        return Business::create($attributes);
-    }
-
-    /**
-     * Create bunch of businesses for single user
-     *
-     * @param integer $user_id business owner id 
-     * @param integer $number number of businesses to create
-     * @return array of App\Model\Business
-     */
-    public function createBusinesses($user_id, $count) {
-        $businesses = array();
-        for ($i = 0; $i < $count; ++$i) {
-            $businesses[] = $this->createBusiness($user_id);
+    public function __call($method, $arguments) {
+        if (!strncmp($method, $this->fixtures->methodPrefix, strlen($this->fixtures->methodPrefix))) {
+            $method = new ReflectionMethod('App\\Ext\\Fixtures', $method);
+            return $method->invokeArgs($this->fixtures, $arguments);
         }
-
-        return $businesses;
-    }
-
-    /**
-     * Create product for specific business
-     * 
-     * @param integer $business_id
-     * @param array $attributes product attrs
-     * @return App\Model\Product
-     */
-    public function createProduct($business_id, $attributes = array()) {
-        $attributes = array_merge(array(
-            'business_id' => $business_id,
-            'name'        => $this->faker->name
-        ), $attributes);
-
-        return Product::create($attributes);
-    }
-
-    /**
-     * Create bunch of products
-     *
-     * @param integer $business_id
-     * @param integer $count number of products
-     */
-    public function createProducts($business_id, $count) {
-        $products = array();
-        for ($i = 0; $i < $count; ++$i) {
-            $products[] = $this->createProduct($business_id);
-        }
-
-        return $products;
     }
 
     public function clearDb() {
